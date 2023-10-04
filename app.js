a4068dacaa5bdee9881d9763b736d1594c5e9823
codeph-0bia <codephobia642@gmail.com>
2023-10-04 17:58:55 +0530

    css and static resr

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"))
+
+let items = [];
 
 app.get("/", (req, res) => {
     let today = new Date();
@@ -17,12 +20,14 @@ app.get("/", (req, res) => {
     let day = today.toLocaleDateString("en-US", options);
 
 
-    res.render('list', { day: day });
+    res.render('list', { day: day, newListItems: items });
 
 });
 
 app.post("/", (req, res) => {
     let newItem = req.body.newTask;
+    if (newItem != "") items.push(newItem);
+    res.redirect("/");
 
 });
 
